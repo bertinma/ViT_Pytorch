afd0e142335679770b9399bda7671b6b011cb030
bertinma <bertin_ma@icloud.com>
2022-09-08 23:33:49 +0200

    init vit model

--- train.py
+++ train.py
@@ -1,26 +1,86 @@
-from d2l import torch as d2l
-from model import transformer
 import argparse
+
 import torch 
+from torch.optim import Adam
+from torch.nn import CrossEntropyLoss
 
-def train(opt):
-    pass
+from utils import load_dataset
+from model.transformer import ViT 
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n-patches', type=int, default=7)
+    parser.add_argument('--hidden-dim', type=int, default=16)
+    parser.add_argument("--epochs", type=int, default=10)
+    parser.add_argument("--batch_size", type=int, default=8)
+    parser.add_argument("--lr", type=float, default=1e-3)
+    parser.add_argument("--weight_decay", type=float, default=1e-4)
+    parser.add_argument("--save_path", type=str, default="weights/vit.pt")
+    parser.add_argument("--device", type=str, default="cpu")
+    return parser.parse_args()
+
+
+def train(opt, model, train_loader, test_loader):
+    optimizer = Adam(model.parameters(), lr=opt.lr, weight_decay=opt.weight_decay)
+    criterion = CrossEntropyLoss()
+
+    for epoch in range(opt.epochs):
+        train_loss = 0.0
+        correct, total = 0, 0 
+        for batch in train_loader:
+            x, y = batch
+            x = x.to(opt.device)
+            y = y.to(opt.device)
+
+            y_hat = model(x)
+            loss = criterion(y_hat, y) / len(x)
+            train_loss += loss.item()
+
+            total += len(x)
+            correct += torch.sum((torch.argmax(y_hat.data, dim=1) == y)).item()
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        print(f'Epoch: {epoch+1}/{opt.epochs},')
+        test_correct, test_total = 0, 0 
+        test_loss = 0.0
+        for batch in test_loader:
+            x, y = batch
+            x = x.to(opt.device)
+            y = y.to(opt.device)
+
+            y_hat = model(x)
+            loss = criterion(y_hat, y) / len(x)
+            test_loss += loss.item()
+
+
+
+            test_total += len(x)
+            test_correct += torch.sum((torch.argmax(y_hat.data, dim=1) == y)).item()
+
+        print(f'Epoch: {epoch+1}/{opt.epochs}, Train Loss: {train_loss}, Train Accuracy: {correct/total*100:.2f}%,  \
+            Test Loss: {test_loss}, Accuracy: {test_correct/test_total*100:.2f}%')
+        
 def save_model(model, path="weights/vit.pt"):
     torch.save(model.state_dict(), path)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--img-size', type=int, default=96, help="Size of image")
-    parser.add_argument('--patch-size', type=int, default=16, help="Size of patches")
-    parser.add_argument('--num-hiddens', type=int, default=512)
-    parser.add_argument('--mlp-num-hiddens', type=int, default=2048)
-    parser.add_argument('--num-heads', type=int, default=8, help="Number of head attentions")
-    parser.add_argument('--num-blocks', type=int, default=2, help="Number of blocks")
-    parser.add_argument('--emb-dropout', type=float, default=.1, help="Embedded dropout")
-    parser.add_argument('--block-dropout', type=float, default=.1, help="Block dropout")
-    parser.add_argument('--lr', type=float, default=.1, help="Learning rate")
-    opt = parser.parse_args()
-    
-    model = train(opt)
-    save_model(model)
+    # Load MNIST dataset into DataLoader
+    # train_load, test_load = load_dataset() 
+
+    # Get arguments
+    opt = get_args()
+
+    # Load model
+    model = ViT(
+        input_shape=(1, 28, 28),
+        n_patches=opt.n_patches,
+        hidden_dim=opt.hidden_dim
+    )
+    x = torch.randn(8, 1, 28, 28)
+    y = model(x)
+    print(y.shape)
+
+    # Train model
+    # train(opt, model, train_load, test_load)
