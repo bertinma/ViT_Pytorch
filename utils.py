eae9eff2a59a935cd68085156b49e20fa2d99ccf
bertinma <bertin_ma@icloud.com>
2022-10-05 15:56:19 +0200

    fix : load eval dataset

--- utils.py
+++ utils.py
@@ -4,12 +4,20 @@ from torchvision.datasets.mnist import MNIST
 from torchvision.transforms import ToTensor
 
 
-def load_dataset(batch_size):
+def load_dataset(batch_size, eval = False):
     transform = ToTensor()
-    train_set = MNIST(root='./datasets/', train=True, download=True, transform=transform)
+    if not eval:
+        train_set = MNIST(root='./datasets/', train=True, download=True, transform=transform)
+        train_load = DataLoader(train_set, shuffle=True, batch_size=batch_size)   
+        shuffle = False 
+    else:
+        batch_size=1 
+        shuffle = True
+
     test_set = MNIST(root='./datasets/', train=False, download=True, transform=transform)
+    test_load = DataLoader(test_set, shuffle=shuffle, batch_size=batch_size)
 
-    train_load = DataLoader(train_set, shuffle=True, batch_size=batch_size)
-    test_load = DataLoader(test_set, shuffle=False, batch_size=batch_size)
-
-    return train_load, test_load
+    if not eval:
+        return train_load, test_load
+    else:
+        return test_load
